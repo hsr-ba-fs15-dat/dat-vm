2f0a6c665039a1d192667505b9d824b1758609f4
Remo Liebi <rliebi@hsr.ch>
2015-02-24 11:34:02 +0100

    vagrant first commit
    #DAT-28 #time 3h

---
+++ puphpet/files/exec-once/install-dat.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install -y nodejs nodejs-legacy npm vim
+sudo npm install -g npm@latest
+    sudo npm install -g dat superlevel gasket datscript
+    source ~/.bashrc
