3517fa84e58932e38629ed426fc055c784549f35
Remo Liebi <rliebi@hsr.ch>
2015-02-25 17:02:57 +0100

    DAT-19 #time 30min introduced Deployment, Removed DAT

--- puphpet/files/exec-once/install-dat.sh
+++ puphpet/files/exec-once/install-dat.sh
@@ -1,5 +1,4 @@
+#!/bin/sh
 sudo apt-get update
-sudo apt-get install -y nodejs nodejs-legacy npm vim
-sudo npm install -g npm@latest
-    sudo npm install -g dat superlevel gasket datscript
-    source ~/.bashrc
+sudo apt-get upgrade
+source ~/.bashrc
