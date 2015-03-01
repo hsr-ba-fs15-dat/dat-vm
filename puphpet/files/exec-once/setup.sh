a883be1196876ef286e8d3ad9b658e4efd0f65ed
Fabio Scala <fabio.scala@gmail.com>
2015-03-01 19:56:15 +0100

    DAT-19 install deps on provision

--- puphpet/files/exec-once/setup.sh
+++ puphpet/files/exec-once/setup.sh
@@ -1,8 +1,35 @@
 #!/bin/sh
+
 sudo apt-get update
 sudo apt-get upgrade
 source ~/.bashrc
 sudo wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
-# required for SASS
+
+# ------ Frontend
+cd /vagrant/webapp/frontend
+
+# Required for SASS
 sudo `which gem` install compass
+
+# Cleanup
+rm -rf bower_components node_modules
+
+# Install deps
+
+npm install
+bower install --config.analytics=false --allow-root
+
+
+# ------ Backend
+cd /vagrant/webapp/backend
+
+# Cleanup
+rm -rf env target
+
+# Create virtualenv with existing packages (see config.yaml)
+virtualenv --system-site-packages env
+source env/bin/activate
+
+# Install all requirements.txt and requirements_dev.txt dependencies
+pyb install_dependencies
