93ce917eb6f63a8a18108e84710230ebbfb92baa
Fabio Scala <fabio.scala@gmail.com>
2015-03-03 09:22:51 +0100

    DAT-44 add createdb permission for opendatahub user

--- puphpet/files/exec-once/setup.sh
+++ puphpet/files/exec-once/setup.sh
@@ -7,3 +7,6 @@ sudo wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # Required for SASS
 sudo `which gem` install compass
+
+export PGPASSWORD=123
+psql -U postgres -h localhost -c 'ALTER USER opendatahub CREATEDB;'
